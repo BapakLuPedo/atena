1536441f39ad59b68fd3aade36f1fc6e4348f835
KhaelSan <80229585+KhaelSan@users.noreply.github.com>
2021-09-19 18:42:01 +0900

    Saotome Atena

---
+++ config.js
@@ -0,0 +1,56 @@
+let fs = require('fs')
+global.owner = ['6282248192917'] // Letakan nomor kamu disini
+global.mods = [] // Moderator?
+global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
+global.APIs = { // API Prefix
+  // nama: 'https://website'
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  jonaz: 'https://jonaz-api-v2.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zekais: 'http://zekais-api.herokuapp.com',
+  zeks: 'https://api.zeks.me',
+  vhtear: 'https://api.vhtear.com',
+  lolhum: 'https://api.lolhuman.xyz',
+  fxc7: 'https://fxc7-api.herokuapp.com',
+  aqul: 'https://api.justaqul.xyz'
+
+}
+global.APIKeys = { // APIKey nya disini
+  // 'https://website': 'apikey'
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://pencarikode.xyz': 'paiz',
+  'https://api.xteam.xyz': 'apikey lu',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.me': 'ShiroNeko',
+  'https://api.lolhuman.xyz': 'HIRO',
+  'https://api.vhtear.com': 'apikey lu',
+  'https://fxc7-api.herokuapp.com': 'apikey lu',
+  'https://api.justaqul.xyz': 'apikey lu'
+}
+
+// Sticker WM
+global.packname = 'Atena wangy'
+global.author = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+
+global.wait = '「❕」 _WAIT LOADING_'
+global.eror = '「❗」 *SERVER ERROR*'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
+global.watermark = '©Atena-san'
+
+global.multiplier = 89 // Semakin tinggi, semakin sulit naik level
+
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
+
+// masukin apikey lu yang ada tulisan 'apikey lu' nya
