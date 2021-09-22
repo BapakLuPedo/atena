1806ae109764a8592d9c6eb35df23ede3d0e3773
KhaelSan <80229585+KhaelSan@users.noreply.github.com>
2021-09-22 13:29:17 +0900

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -1,13 +1,25 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, usedPrefix }) => {
-	let res = await fetch(global.API('lolhum', '/api/random/waifu', 'apikey'))
-	if (!res.ok) throw await res.text()
-	let img = await res.buffer()
-	if (!img) throw img
-		conn.sendButtonImg(m.chat, await(await fetch(img)).buffer(), 'Nih waifu nya', watermark, '⏩Get Again', `${usedPrefix}waifu`, m)
-	}
-handler.help = ['waifu']
-handler.tags = ['internet']
-handler.command = /^(waifu)$/i
-
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, usedPrefix }) => {
+
+	let res = await fetch(global.API('lolhum', '/api/random/waifu', {}, 'apikey'))
+
+	if (!res.ok) throw await res.text()
+
+	let img = await res.buffer()
+
+	if (!img) throw img
+
+		conn.sendButtonImg(m.chat, await(await fetch(img)).buffer(), 'Nih waifu nya', watermark, '⏩Get Again', `${usedPrefix}waifu`, m)
+
+	}
+
+handler.help = ['waifu']
+
+handler.tags = ['internet']
+
+handler.command = /^(waifu)$/i
+
+
+
 module.exports = handler
