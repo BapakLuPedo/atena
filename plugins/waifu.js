e9666c985c326bb3a600961997ca27974a9b9895
KhaelSan <80229585+KhaelSan@users.noreply.github.com>
2021-09-22 13:25:19 +0900

    Add files via upload

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix }) => {
 	let res = await fetch(global.API('lolhum', '/api/random/waifu', 'apikey'))
-	if (!res.ok) throw eror
+	if (!res.ok) throw await res.text()
 	let img = await res.buffer()
 	if (!img) throw img
 		conn.sendButtonImg(m.chat, await(await fetch(img)).buffer(), 'Nih waifu nya', watermark, '⏩Get Again', `${usedPrefix}waifu`, m)
