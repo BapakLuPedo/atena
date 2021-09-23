bfdf5560230bb97905d15d4ddcc35cba777a162b
KhaelSan <80229585+KhaelSan@users.noreply.github.com>
2021-09-23 14:53:34 +0900

    update gacha.js

--- plugins/gacha.js
+++ plugins/gacha.js
@@ -18,26 +18,11 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 ├ wallnime
 └────
 
-┌〔 List Gacha-Nsfw 〕
-├ 
-├ 
-├ 
-├ 
-├ 
-├ 
-├ 
-├ 
-├ 
-├ 
-└────
-
 example:
 ${usedPrefix + command} elf
     `.trim()
-		if (!args[0]) throw er
 
     switch (args[0].toLowerCase()) {
-    	/*Gacha time*/
         case 'waifu':
         case 'husbu':
         case 'neko':
@@ -46,7 +31,7 @@ ${usedPrefix + command} elf
         case 'shota':
         case 'sagiri':
         case 'kanna':
-        case 'elaina': /*wangy wangy hu ha hu ha🥵*/
+        case 'elaina':
         case 'shinobu':
         case 'megumin':
         case 'art':
@@ -56,36 +41,8 @@ ${usedPrefix + command} elf
 			if (!res.ok) throw await res.text()
 			let img = await res.buffer()
 			if (!img) throw img
-			conn.sendButtonImg(m.chat, await(img), 'Nih ' + args[0].toLowerCase() + ' nya', watermark, '⏩Get Again', `${usedPrefix}gacha ` + args[0].toLowerCase(), m)
+				conn.sendButtonImg(m.chat, await(img), 'Nih ' + args[0].toLowerCase() + ' nya', watermark, '⏩Get Again', `${usedPrefix}gacha ` + args[0].toLowerCase(), m)
             break
-		/*Horny time*/
-				case 'chiisaihentai':
-				case 'trap':
-                case 'blowjob':
-                case 'yaoi':
-                case 'ecchi':
-                case 'hentai':
-                case 'ahegao':
-                case 'hololewd':
-                case 'sideoppai':
-                case 'animefeets':
-                case 'animebooty':
-                case 'animethighss':
-                case 'hentaiparadise':
-                case 'animearmpits':
-                case 'hentaifemdom':
-                case 'lewdanimegirls':
-                case 'biganimetiddies':
-                case 'animebellybutton':
-                case 'hentai4everyone':
-		/*if(!handler.nsfw === true ) return m.reply('「❗」 *Fitur Nsfw Mati*') // or if(handler.nsfw === false ) return m.reply('「❗」 *Fitur Nsfw Mati*')*/
-			let res = await fetch(global.API('lolhum', '/api/random/nsfw/' + args[0].toLowerCase(), {}, 'apikey'))
-			m.reply(global.wait)
-				if (!res.ok) throw await res.text()
-				let img = await res.buffer()
-				if (!img) throw img
-					conn.sendButtonImg(m.chat, await(img), 'Hayoo ngapain pegang tytyd', watermark, '💦Ngocok Lagi', `${usedPrefix}gacha ` + args[0].toLowerCase(), m)
-					break
         default:
             throw er
     }
