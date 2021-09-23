3d82e65f8bb57958f72927728ae3a1647dd5e867
KhaelSan <80229585+KhaelSan@users.noreply.github.com>
2021-09-23 10:36:43 +0900

    Fix waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -1,25 +1,16 @@
 let fetch = require('node-fetch')
-
 let handler = async (m, { conn, usedPrefix }) => {
-
 	let res = await fetch(global.API('lolhum', '/api/random/waifu', {}, 'apikey'))
-
-	if (!res.ok) throw await res.text()
-
-	let img = await res.buffer()
-
-	if (!img) throw img
-
-		conn.sendButtonImg(m.chat, await(await fetch(img)).buffer(), 'Nih waifu nya', watermark, '⏩Get Again', `${usedPrefix}waifu`, m)
-
+	m.reply(global.wait)
+    if (!res.ok) throw await res.text()
+    let img = await res.buffer()
+    if (!img) throw img
+    conn.sendButtonImg(m.chat, await(img), 'Nih waifu nya', watermark, '⏩Get Again', `$waifu`, m)
 	}
 
 handler.help = ['waifu']
-
 handler.tags = ['internet']
-
 handler.command = /^(waifu)$/i
-
 
 
 module.exports = handler
