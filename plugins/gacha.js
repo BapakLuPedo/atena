d571e4e3f2c4095df991aa5a889a538a1dfde759
KhaelSan <80229585+KhaelSan@users.noreply.github.com>
2021-09-23 11:42:30 +0900

    Fadli mastah 🛐

--- plugins/gacha.js
+++ plugins/gacha.js
@@ -2,28 +2,41 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     let er = `
-┌〔 Gacha 〕
+┌〔 List Gacha 〕
 ├ waifu
+├ husbu
 ├ neko
 ├ loli
 ├ elf
+├ shota
+├ sagiri
+├ shinobu
+├ megumin
+├ wallnime
 └────
 
 example:
 ${usedPrefix + command} elf
     `.trim()
+		if (!args[0]) throw er
 
     switch (args[0].toLowerCase()) {
         case 'waifu':
+        case 'husbu':
         case 'neko':
         case 'loli':
         case 'elf':
+        case 'shota':
+        case 'sagiri':
+        case 'shinobu':
+        case 'megumin':
+        case 'wallnime':
 		let res = await fetch(global.API('lolhum', '/api/random/' + args[0].toLowerCase(), {}, 'apikey'))
 		m.reply(global.wait)
 			if (!res.ok) throw await res.text()
 			let img = await res.buffer()
 			if (!img) throw img
-				conn.sendButtonImg(m.chat, await(img), 'Nih ' + args[0].toLowerCase() + ' nya', watermark, '⏩Get Again', `${usedPrefix}gacha args[0].toLowerCase()`, m)
+				conn.sendButtonImg(m.chat, await(img), 'Nih ' + args[0].toLowerCase() + ' nya', watermark, '⏩Get Again', `${usedPrefix}gacha ` + args[0].toLowerCase(), m)
             break
         default:
             throw er
