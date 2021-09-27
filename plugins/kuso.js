fb36278b7bac4b2aabc4d67a4770932faff9bde6
KhaelSan <80229585+KhaelSan@users.noreply.github.com>
2021-09-27 10:37:15 +0900

    Saotome Atena

--- plugins/kuso.js
+++ plugins/kuso.js
@@ -4,9 +4,9 @@ let handler = async (m, {conn, command, text, usedPrefix }) => {
 	if(!text) throw `uhm.. cari anime apa?\n\nContoh:\n${usedPrefix + command} Tejina senpai`
 	let res = await fetch(`https://api.lolhuman.xyz/api/kusonimesearch?apikey=pemburuBansos&query=${text}`)
 	let { title, thumbnail, japanese, genre, seasons, producers, type, status, total_episode, score, duration, released_on, link_dl } = res
-			let img = await fetch(thumbnail)
+			//let img = await fetch(thumbnail)
 			//if (!img) throw img
-		//await m.reply(global.wait)
+		await m.reply(global.wait)
 			if (!res.ok) throw await res.text()
 					ini_txt = `*Title :* ${title}\n`
 					ini_txt += `*Japanese :* ${japanese}\n`
